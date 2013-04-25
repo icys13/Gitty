362a8cde5e0e35cd079404b40cf09ec528deabf5
icys13 <jiajun.li13@gmail.com>
2013-04-25 14:16:15 +0800

    fifth commit 注册模块重写.

--- application/controllers/install.php
+++ application/controllers/install.php
@@ -46,52 +46,13 @@ class Install extends CI_Controller {
 		if($msg['flag'])
 		{
 			// 创建成功
-			$msg['error']= '数据库创建成功,请完善管理员信息.';
-			$this->register($msg);
+			redirect(site_url('regist/index'));
 		}
 		else
 			//建立不成功
 			$this->load->view('readme',$msg['remind']);
 	}
 
-	public function register($msg='')
-	{
-
-		// 收集 post 提交的数据
-		$data['username'] = $this->input->post('username');
-		$data['password'] = $this->input->post('password');
-		$data['confirm'] = $this->input->post('confirm');
-		$data['email'] = $this->input->post('email');
-		$data['ssh_key'] = $this->input->post('ssh_key');
-		$data['date'] = $this->input->post('date');
-		$data['location'] = $this->input->post('location');
-		$data['img'] = $this->input->post('img');
-		$data['admin'] = $this->input->post('admin');
-
-		$this->load->model('user_model');
-
-		if($this->user_model->is_empty($data))
-		{
-			$this->load->view('header');
-			$this->load->view('register',$msg);
-			$this->load->view('footer');
-		}
-		else
-		{
-			$msg = $this->user_model->check($data);
-			if($msg['flag'])
-			{
-				$this->user_model->insert($data);
-				redirect(site_url('admin/index'));
-			}
-			else
-			{
-				$this->load->view('header');
-				$this->load->view('register',$msg);
-				$this->load->view('footer');
-			}
-		}
-	}
 }
 
 /* End of file install.php */
