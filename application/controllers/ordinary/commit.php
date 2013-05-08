7bf76ecd06f18beef0d1df68a116d000d27db403
icys13 <jiajun.li13@gmail.com>
2013-05-08 15:23:16 +0800

    45th commit ---commits 信息按时间分类

--- application/controllers/ordinary/commit.php
+++ application/controllers/ordinary/commit.php
@@ -19,20 +19,33 @@ class Commit extends Users {
 		$msg['commits_title'] = $data[0]['commit'];
 
 		// 显示提交历史
-		$msg['commits'] = '';
 		$size = count($data);
-		for($i = 0;$i < $size;$i++)
+
+		// 日期初始化 
+		$date = date("Y-m-d",strtotime($data[0]['date']));
+		$msg['commits'] = '<div class="commit-group">';
+		$msg['commits'] .= '<h3>'.$date.'</h3>';
+
+		for($i = 0;$i < $size;)
 		{
-			//print_r($item);
-			$msg['commits'] .= '<div class="commit-group">';
-			$msg['commits'] .= '<h3>'.date("Y-m-d",strtotime($data[$i]['date'])).'</h3>';
-			$msg['commits'] .= '<ol><li><div class="commit-desc">';
-			$msg['commits'] .= '<span class="username"><a href="">'.$data[$i]['committer'].'</a></span> : ';
-			$msg['commits'] .= '<a>'.$data[$i]['message'].'</a></div>';
-			$msg['commits'] .= '<div class="commit-meta fr">';
-			$msg['commits'] .= '<a class="sha" href="'.base_url().'index.php/ordinary/commit/detail/'.$username.'/'.$reponame.'/'.$data[$i]['commit'].'"><i class="icon-push"></i>'.substr($data[$i]['commit'],0,8).'</a>';
-			$msg['commits'] .= '</div></li></ol></div>';
+			if($date == date("Y-m-d",strtotime($data[$i]['date'])))
+			{
+				$msg['commits']	.= '<li><div class="commit-desc">';
+				$msg['commits'] .= '<span class="username"><a href="">'.$data[$i]['committer'].'</a></span> : ';
+				$msg['commits'] .= '<span>'.$data[$i]['message'].'</span></div>';
+				$msg['commits'] .= '<div class="commit-meta fr">';
+				$msg['commits'] .= '<a class="sha" href="'.base_url().'index.php/ordinary/commit/detail/'.$username.'/'.$reponame.'/'.$data[$i]['commit'].'"><i class="icon-push"></i>'.substr($data[$i]['commit'],0,8).'</a>';
+				$msg['commits'] .='</div></li>';
+				$i++;
+			}
+			else
+			{
+				$date = date("Y-m-d",strtotime($data[$i]['date']));
+				$msg['commits'] .= '</ol></div><div class="commit-group">';
+				$msg['commits'] .= '<h3>'.$date.'</h3>';
+			}
 		}
+		$msg['commits'] .= '</ol></div>';
 
 		// 载入模板
 		$this->load->view('header');
